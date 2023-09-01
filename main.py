de99c5755de481fa9ebe776fad690ebcce35696c
tinayiluo0322 <143360909+tinayiluo0322@users.noreply.github.com>
2023-08-31 19:49:28 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
 """
-Main cli or app entry point
+Main code
 """
 
-from mylib.calculator import add
-import click
-
-
-@click.command("add")
-@click.argument("a", type=int)
-@click.argument("b", type=int)
-def add_cli(a, b):
-    click.echo(add(a, b))
-
-
-if __name__ == "__main__":
-    # pylint: disable=no-value-for-parameter
-    add_cli()
+#if __name__ == "__main__":
+   
