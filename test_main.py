4bc93332896d666d69acc86d58adb099c6d1e0e2
tinayiluo0322 <143360909+tinayiluo0322@users.noreply.github.com>
2023-09-01 09:13:41 -0700

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -2,4 +2,26 @@
 Test goes here
 
 """
+from main import positive_real_number
+
+def test_positive_real_number():
+    # Test with a positive number
+    result = positive_real_number(5)
+    assert result is True, "Test failed for the positive number"
+
+    # Test with zero (non-positive number)
+    result = positive_real_number(0)
+    assert result is False, "Test failed for zero (non-positive number)"
+
+    # Test with a negative number
+    result = positive_real_number(-5)
+    assert result is False, "Test failed for the negative number"
+
+if __name__ == '__main__':
+    test_positive_real_number()
+
+
+
+
+
 
